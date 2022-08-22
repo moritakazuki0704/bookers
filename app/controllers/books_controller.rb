f6996517fb609ad7d4794d509fa9ee843aa9a47e
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2022-08-22 18:51:44 +0000

    [add]3-12 layout format

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,13 +22,15 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to books_path
+    redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+     flash[:notice] = "Book was successfully destroyed."
+     redirect_to books_path
+    end
   end
 
   private
